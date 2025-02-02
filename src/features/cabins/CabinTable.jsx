5a9372a4ef34afa96aaaa2ab96c094d359940fd5
Alim1899 <alimakhchanogli@gmail.com>
2025-02-03 02:05:16 +0400

    Implemented react-query

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import getCabins from "../../services/apiCabins";
+import CabinRow from "./CabinRow";
+import Spinner from "../../ui/Spinner";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -23,3 +27,31 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+const CabinTable = () => {
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />;
+  if (error) throw new Error("Error during loading data");
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div> <div>Cabin</div> <div>Capacity</div> <div>Price</div>{" "}
+        <div>Discound</div> <div></div>
+      </TableHeader>
+      {cabins &&
+        cabins.map((cabin) => {
+          return <CabinRow key={cabin.id} cabin={cabin} />;
+        })}
+    </Table>
+  );
+};
+
+export default CabinTable;
