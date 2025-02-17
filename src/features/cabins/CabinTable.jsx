31cd2d91bba840516a007da7d2810db1abb7485e
Alim1899 <alimakhchanogli@gmail.com>
2025-02-17 20:01:52 +0400

    Sorted and filtered cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,9 +3,27 @@ import Spinner from "../../ui/Spinner";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 const CabinTable = () => {
   const { isLoading, cabins, error } = useCabins();
+  const [searchParams] = useSearchParams();
   if (isLoading) return <Spinner />;
+  // 1. FILTER
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabins;
+
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  // 2. SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
   if (error) throw new Error("Error during loading data");
   return (
     <Menus>
@@ -16,7 +34,7 @@ const CabinTable = () => {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={sortedCabins}
           render={(cabin) => {
             return <CabinRow key={cabin.id} cabin={cabin} />;
           }}
---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,28 @@
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
+const CabinTableOperations = () => {
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No-discount" },
+          { value: "with-discount", label: "With-discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-des", label: "Sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort by Price(low first" },
+          { value: "regularPrice-des", label: "Sort by Price(high first" },
+          { value: "maxCapacity", label: "Sort by Capacity" },
+        ]}
+      ></SortBy>
+    </TableOperations>
+  );
+};
+
+export default CabinTableOperations;
